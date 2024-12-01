db7b659006a534f7139ef00c109565291d7a73d6
Tusharjamdade <tusharnjamdade@gmail.com>
2024-12-01 20:05:46 +0000

    added tailwind css

--- app/index.tsx
+++ app/index.tsx
@@ -1,42 +1,10 @@
-import { Link } from 'expo-router';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { useColorScheme } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function App() {
-  const colorScheme = useColorScheme(); // 'light' or 'dark'
-
-  const isDarkMode = colorScheme === 'light';
-  const styles = isDarkMode ? darkStyles : lightStyles;
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Dark Mode!</Text>
-      
+    <View className="bg-red">
+      <Text className="text-red-500">Hi there, this is tushar</Text>
     </View>
   );
 }
-
-const lightStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#000',
-  },
-});
-
-const darkStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './app.{js,jsx,ts,tsx}', 
+    './app/**/*.{js,jsx,ts,tsx}', // Add your custom directories
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
